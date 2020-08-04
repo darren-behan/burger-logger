a21e812369872905a545dc47c738a0d5e5b458a4
Darren Behan <darrenbehan@hotmail.com>
2020-08-04 16:26:53 +1000

    added content to schema.sql & seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devoured) VALUES ('Beef', true);
+INSERT INTO burgers (name, devoured) VALUES ('Chicken', true);
+INSERT INTO burgers (name, devoured) VALUES ('Veggie', true);
+
