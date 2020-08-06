114ae3bc373f8f633f99fd7d188d487462a3316d
Darren Behan <darrenbehan@hotmail.com>
2020-08-06 18:50:07 +1000

    adding button to uneaten burgers - functionality to move to eaten required

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -18,4 +18,25 @@ $(function() {
       }
     );
   });
+
+  $(".devour").on("click", function(event) {
+    var id = $(this).data("id");
+    var newSleep = $(this).data("newsleep");
+
+    var newSleepState = {
+      sleepy: newSleep
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/cats/" + id, {
+      type: "PUT",
+      data: newSleepState
+    }).then(
+      function() {
+        console.log("changed sleep to", newSleep);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
 });
